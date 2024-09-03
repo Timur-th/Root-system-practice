7259e4e5ffca173d7fe249a27bdaaf2a680af8d2
Timur Aliev <156211396+Timur-th@users.noreply.github.com>
2024-09-03 23:09:20 +0300

    Update checking_ch_v.cpp

--- checking_ch_v.cpp
+++ checking_ch_v.cpp
@@ -42,7 +42,6 @@ int main() {
         }
     }
 
-    int count = 0;
     for (int i1 = 0; i1 < 8 - 2; ++i1) {
         for (int i2 = i1 + 1; i2 < 8 - 1; ++i2) {
             for (int i3 = i2 + 1; i3 < 8 ; ++i3) {
@@ -58,7 +57,6 @@ int main() {
                             if (res) {
                                 std::cout << "Yes" << '\n';
 //                                cout << i1 << ' ' << i2 << ' ' << i3 << ' ' << i4 << ' ' << i5 << ' ' << i6;
-                                ++count;
 //                            }
 //                        }
 //                    }
@@ -66,6 +64,4 @@ int main() {
             }
         }
     }
-
-    std::cout << count;
 }
